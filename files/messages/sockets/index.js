4b3f6445f83b26855d506a341a07ed40df11a579
codewithemmy <codewithemmy@gmail.com>
2023-09-15 22:27:49 +0100

    update

--- files/messages/sockets/index.js
+++ files/messages/sockets/index.js
@@ -20,30 +20,28 @@ module.exports.socketConnection = async (io) => {
       else io.emit("display", data)
     })
 
-    socket.on("join", async (obj) => {
-      try {
-        //check to delete the socket id with the userId
-        await SocketRepository.deleteMany({
-          userId: new mongoose.Types.ObjectId(obj.userId),
-        })
+    try {
+      //check to delete the socket id with the userId
+      await SocketRepository.deleteMany({
+        userId: new mongoose.Types.ObjectId(obj.userId),
+      })
 
-        //create a new socket
-        const socketDetails = await SocketRepository.createSocket({
-          socketId: socket.id,
-          userId: obj.userId,
-          modelType: obj.type,
-        })
+      //create a new socket
+      const socketDetails = await SocketRepository.createSocket({
+        socketId: socket.id,
+        userId: obj.userId,
+        modelType: obj.type,
+      })
 
-        //emit error sockets is not generated
-        if (!socketDetails._id) {
-          socket.emit("join", `Error: ${data.message}`)
-        } else {
-          socket.emit("join", "Connection Successful")
-        }
-      } catch (error) {
-        console.log("socket error", error)
+      //emit error sockets is not generated
+      if (!socketDetails._id) {
+        socket.emit("join", `Error: ${data.message}`)
+      } else {
+        socket.emit("join", "Connection Successful")
       }
-    })
+    } catch (error) {
+      console.log("socket error", error)
+    }
 
     socket.on("disconnect", async () => {
       await SocketRepository.deleteUser(socket.id)
