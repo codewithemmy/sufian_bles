f063aa6d18e5e3c5d96ae9c8e6d5c2baccf50e50
codewithemmy <codewithemmy@gmail.com>
2023-10-01 13:21:22 +0100

    fixes: image resolution

--- utils/multer.js
+++ utils/multer.js
@@ -9,12 +9,16 @@ cloudinary.config({
   api_secret: config.CLOUDINARY_API_SECRET,
 })
 
+const width = 450
+const height = 330
+
 const uploadManager = (destination) => {
   return multer({
     storage: new CloudinaryStorage({
       cloudinary: cloudinary,
       params: {
-        folder: `Citifix/${destination}`,
+        folder: `BlesClient/${destination}`,
+        transformation: [{ width: width, height: height }],
       },
     }),
   })
